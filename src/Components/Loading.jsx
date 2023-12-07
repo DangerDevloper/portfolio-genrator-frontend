e6e58a2296ffa34fd9f061a33488c57a09c2fcb5
DangerDevloper <153118359+DangerDevloper@users.noreply.github.com>
2023-12-07 23:25:34 +0530

    Update Loading.jsx

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,15 +1,19 @@
-
+import "./Loading.css"
 
 const Loading = () => {
     
     return (
-      <div style={{ width: "100%", height: "100%" }}>
-        <div style={{display:"flex",justifyContent:"center", backgroundColor: "0F0E17" }}>
-          <img
-            width="100%"
-            src="https://cdn.dribbble.com/users/4358240/screenshots/14660462/media/45a8935c98e3afdb215e9df9313467dd.gif"
-            alt="loading"
-          />
+      <div class="content">
+        <div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
